b281f276f36608ee7baac043818ab92ebd0d82d6
qzart45hi <qwert45hi@yandex.ru>
2022-04-30 19:00:20 +0300

    feat: update moderation submodule

--- connect.py
+++ connect.py
@@ -2,12 +2,12 @@ from datetime import timedelta
 
 from common import app
 from common import db_meta, db_url  # noqa  # for alembic
-from moderation import mub_cli_blueprint, superuser_namespace
-
+from moderation import mub_cli_blueprint, superuser_namespace, mub_base_namespace
 
 app.register_blueprint(mub_cli_blueprint)
 jwt = app.configure_jwt_with_loaders(["cookies"], timedelta(hours=72), print)
 api = app.configure_restx()
 
+api.add_namespace(mub_base_namespace)
 api.add_namespace(superuser_namespace)
 # add your namespaces here via `api.add_namespace(<namespace>)`
