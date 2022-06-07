c774995d92624564f04a5bb5391e6aa3fc5748bb
qzart45hi <qwert45hi@yandex.ru>
2022-06-08 02:43:15 +0300

    fix: jwt config for secure samesite cookies

--- connect.py
+++ connect.py
@@ -5,7 +5,8 @@ from common import db_meta, db_url  # noqa  # for alembic
 from moderation import mub_cli_blueprint, superuser_namespace, mub_base_namespace
 
 app.register_blueprint(mub_cli_blueprint)
-jwt = app.configure_jwt_with_loaders(["cookies"], timedelta(hours=72), print)
+jwt = app.configure_jwt_with_loaders(["cookies"], timedelta(hours=72), print,
+                                     csrf_protect=False, samesite_cookie="Strict")
 api = app.configure_restx()
 
 api.add_namespace(mub_base_namespace)
